573d7192ba5a3130195482210901d3701e200cfe
schachtjOSU <schachtj@oregonstate.edu>
2016-02-16 11:58:15 -0800

    test2

--- Schachtsick_GETandPOSTchecker/GETandPOSTcheck.js
+++ Schachtsick_GETandPOSTchecker/GETandPOSTcheck.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());*/
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3333);
+app.set('port', 3003);
 
 app.get('/',function(req,res){
   res.render('home');
