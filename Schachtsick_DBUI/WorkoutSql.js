6c15ec2f8516f02cfa1f5e06fe4da0a7afce1b3f
schachtjOSU <schachtj@oregonstate.edu>
2016-03-04 15:32:20 -0800

    Delete

--- Schachtsick_DBUI/WorkoutSql.js
+++ Schachtsick_DBUI/WorkoutSql.js
@@ -30,7 +30,6 @@ app.get('/',function(req,res,next){
 	context.results = JSON.stringify(rows);
 	res.render('home',context);
 	console.log(context.results);
-	
   });
 });
 
@@ -83,6 +82,31 @@ app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
 
+
+function deleteRow(tableID,currentRow) {
+    try {
+        var table = document.getElementById(tableID);
+        var rowCount = table.rows.length;
+        for (var i = 0; i < rowCount; i++) {
+            var row = table.rows[i];
+            /*var chkbox = row.cells[0].childNodes[0];*/
+            /*if (null != chkbox && true == chkbox.checked)*/
+            
+            if (row==currentRow.parentNode.parentNode) {
+                if (rowCount <= 1) {
+                    alert("Cannot delete all the rows.");
+                    break;
+                }
+                table.deleteRow(i);
+                rowCount--;
+                i--;
+            }
+        }
+    } catch (e) {
+        alert(e);
+    }
+    //getValues();
+}
 
 /*function addButton(){
 	document.getElementById('addSubmit').addEventListener('click', function(event){
