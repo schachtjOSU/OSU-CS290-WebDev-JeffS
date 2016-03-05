8785557e21c6f1c0ca4e00c72f82b746ec59890c
schachtjOSU <schachtj@oregonstate.edu>
2016-03-04 16:11:06 -0800

    delete try3

--- Schachtsick_DBUI/WorkoutSql.js
+++ Schachtsick_DBUI/WorkoutSql.js
@@ -49,12 +49,12 @@ app.get('/insert',function(req,res,next){
 // Delete a row
 app.get('/delete', function(req, res, next) {
     var context = {};
-    pool.query("DELETE FROM workouts WHERE id = ?", [req.query.id], function(err, result) {
+    mysql.pool.query("DELETE FROM workouts WHERE id = ?", [req.query.id], function(err, result) {
         if(err){
             next(err);
             return;
         }
-        pool.query('SELECT * FROM workouts', function(err, rows, fields){
+        mysql.pool.query('SELECT * FROM workouts', function(err, rows, fields){
             if(err){
                 next(err);
                 return;
